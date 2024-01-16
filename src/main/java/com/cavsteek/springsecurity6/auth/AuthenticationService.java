94646b13315be1e2ed68f3163679bad2ac70be6c
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-16 14:19:07 +0100

    setup authenticationRegister method

--- src/main/java/com/cavsteek/springsecurity6/auth/AuthenticationService.java
+++ src/main/java/com/cavsteek/springsecurity6/auth/AuthenticationService.java
@@ -1,14 +1,33 @@
 package com.cavsteek.springsecurity6.auth;
 
+import com.cavsteek.springsecurity6.config.JWTService;
+import com.cavsteek.springsecurity6.repository.UserRepository;
+import com.cavsteek.springsecurity6.user.Role;
+import com.cavsteek.springsecurity6.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
